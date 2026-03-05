3aeac530ecb0c0703912325f47d01ca1b9fadde8
Elliott Morris <131560931+Lampfood@users.noreply.github.com>
2026-03-04 21:39:52 -0600

    Week 7 Program 2.py

--- program_2.py
+++ program_2.py
@@ -1,36 +1,29 @@
-# Program #2: Larger than n
-# In a program, write a function (with NO output) that accepts two arguments: number n, and a list.
-# Assume that the list contains numbers.
-# The function shell has been written out on line 30, (def display_larger_than_n_list)
-# and should display all of the numbers in the list that are greater than then number n.
+#Elliott Morris, 2/19/2026, Greater than.py
+
+def display_greater_than(lst,n):
+    #compares values
+    for num in lst:
+        if num > n:
+            print(num)
 
 def main():
-    # Declare local variables
-    number = 5
-    number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    #create a list of numbers
+    numbers = []
 
-    # Display the number.
-    print('Number:', number)
+    #ask user how many numbers they want to enter
+    count = int(input("How many numbers would you like to add? "))
 
-    # Display the list of numbers.
-    print('List of numbers:')
-    print(f'{number_list}')
-    
-    # Display the list of numbers that are larger
-    # than the number.
-    print(f'List of numbers that are larger than {number}:')
-    
-    # Call the display_larger_than_n_list function,
-    # passing a number and number list as arguments.
-    display_larger_than_n_list(number, number_list)
+    #Get numbers from user
+    for i in range(count):
+        num = float(input(f"Enter number {i+1}: "))
+        numbers.append(num)
 
-# The display_larger_than_n_list function accepts two arguments:
-# a list, and a number. The function displays all of the numbers
-# in the list that are greater than the number.
-def display_larger_than_n_list(n, n_list):
-    # Write your code to display all of the numbers in the list that are greater than then number n. below
-    print('In display_larger_than_n_list')
-        
-# Call the main function.
-if __name__ == '__main__':
+    #Get comparison number
+    n = float(input("Enter number to compare against: "))
+
+    #print results
+    print(f"\nNumbers Greater than {n}: ")
+    display_greater_than(numbers,n)
+
+if __name__ == "__main__":
     main()
